021b27756cac147b0777b33fafcf1c125692506c
Syed Ahsan Ali <ahsankazmi2002@gmail.com>
2025-10-15 13:29:56 -0700

    01-Folder done

---
+++ 01-Folder/src/App.jsx
@@ -0,0 +1,27 @@
+// function App(){
+//   return "hello from App.js";
+// }
+
+// export default App;
+
+// Using the fat Arrow function
+const App = () => {
+  return (
+    <> {/*This is fragments*/}
+  <div>
+  <h2>Ahsan</h2>
+  <h1>Hello How are you</h1>
+  </div>
+
+  {/* Now we can create another div  */}
+  <div>
+    <h1>Again hello</h1>
+  </div>
+
+  </>
+
+  // If we want to make another div then we use fragments
+  )
+}
+
+export default App;
